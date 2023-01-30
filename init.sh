5ce65612d6b14f86d8c8d919113ba339b31831fd
Santosh IYengar <siyengar@guardanthealth.com>
2023-01-30 02:27:12 -0800

    Added Github Actions.

--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
 mkdir -p ~/.config/gcloud/
-echo "{
-  "PROJECT_ID": $1
+echo"{
+  "PROJECT_ID": $1,
   "PRIVATE_KEY_ID": $2,
   "PRIVATE_KEY": $3
 }" > ~/.config/gcloud/application_default_credentials.json
